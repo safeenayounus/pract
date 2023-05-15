c41bf6b520b73e1c3c6b618c5a885b59a706e031
safeenayounus <123816603+safeenayounus@users.noreply.github.com>
2023-05-15 23:14:07 +0500

    Add files via upload

---
+++ appjs/chap9.js
@@ -0,0 +1,124 @@
+// // 1
+// let city = +prompt("enter your city name");
+// alert("welcome to the city of light");
+// 2
+// let gender = prompt("enter your gender");
+// if (gender ==="male") {
+//     alert("good morning sir");
+// } else if(gender ==="female") {
+//     alert("good morning mam");
+// }
+// // 3
+// let color = prompt("enter traffic signal color");
+// if (color === "red") {
+//   alert(color  + " must stop");
+// } else if (color  === "green") {
+//   alert(color  +" move now")
+// } else if (color  === "yellow") {
+//   alert(color   + " ready to  move");
+// }
+// 4
+// let liter = prompt(" tell me remaining fuel in your car");
+// if (liter <= 3.5) {
+//   alert("please refill your cars fuel")
+// } else {
+//   alert("chl jae ga");
+// }
+// 5
+//  var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+// var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+//  var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+//  if (true){
+// alert("True");
+// }
+// if (false) {
+//   alert("False");
+// }
+
+// 6
+
+// let prcntage = prompt("enter your recent result percentage")
+//    if (prcntage >= 80) {
+//     alert("acha hy pr is se acha ho skta h ")
+//   } else if (prcntage >= 70) {
+//     alert("tere to lg gae")
+//   } else if (prcntage >= 60) {
+//     alert("apki azmat ko mera salam")
+//   } else if (prcntage >= 50) {
+//    alert("bhai ye lany ki bhi kia zarurt thi")
+//   } else {
+//     alert("bus khtm")
+// }
+
+// 7
+
+// let num = Math.floor(Math.random() * 10) + 1;
+// let guess = parseInt(prompt("Guess the secret number b/w 1 to 10:"));
+// if (guess === num) {
+//   alert("ary wah");
+// } else if (guess === num + 1 || guess === num - 1) {
+//   alert("oooo kareeb tha");
+// } else {
+//   alert("chl agli br");
+// }
+
+// 8
+
+// let num = prompt("enter the number");
+
+// if (num % 3 === 0) {
+//   alert(" your num is divided by 3 ")
+// } else {
+//   alert("not divided by 3")
+// }
+
+// 9
+
+//  let num = prompt("enter the number");
+
+// if (num % 3 === 0) {
+//   alert(" odd number ")
+// } else if ( num % 2 ===0) {
+//   alert("even number")
+// }
+
+// 10
+let temp = prompt("enter the temperarure");
+if (temperature > 40) {
+  alert("It is too hot outside.");
+} else if (temperature > 30) {
+  alert("The Weather today is Normal.");
+} else if (temperature > 20) {
+  alert("Todays Weather is cool.");
+} else if (temperature > 10) {
+  alert("OMG! Todays weather is so Cool.");
+} else {
+  alert("Temperature is too low.");
+}
+
+// 11
+
